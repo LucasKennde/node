bb117933c9a1f8f0eb317c230397ced5feca6a47
Lucas Kennde <lucaskennde@gmail.com>
2024-04-20 22:00:30 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+
+const app = express();
+
+app.get("/", function(req, res){
+    res.send("Hello World");
+
+})
+
+app.get("/user", function(req, res){
+    res.send(nome="José");
+
+})
+
+app.delete("/user", function(req, res){
+    res.send(nome="José deletado");
+
+})
+app.listen(8080,function(){
+
+    console.log('Servidor funcionando')
+})
